6813a4927b3a280fd585bd744a76b5e778c23849
Germaingit <germain.veron@me.com>
2021-03-10 17:07:44 +0100

    ll

--- frontend/descri.js
+++ frontend/descri.js
@@ -1,53 +1,47 @@
-
 ;
 async () => {
-    const teddiesId = getTeddiestId()
-    const teddiesData = await getTeddiesData(teddiesId)
+  const teddiesId = getTeddiesId()
+  const teddiesData = await getTeddiesData(teddiesId)
+
 }
+console.log(getTeddiesId)
 
 function getTeddiesId() {
-    return new URL(window.location.href).searchParams.get('_id')
+  return new URL(window.location.href).searchParams.get('_id')
 }
-
-fetch("http://localhost:3000/api/teddies?${teddy._id}")
+console.log(teddies)
+function getTeddiesData(teddies){
+  return fetch(`http://localhost:3000/api/teddies/${teddiesId}`)
     .then(response => response.json())
-    .then(teddies => {
+    .then(teddiesData => {
         const teddiesElement = document.querySelector('#descrip')
-        for (let teddy of teddies) {
-        console.log(teddy)
+         {
+        console.log(teddies)
         
            
 
-            teddiesElement.innerHTML += 
-                   `<div class="card">
-        
-        <div class="row">
+            teddiesElement.innerHTML += `<div id="" class="card col-6-lg bg-light gap-4 p-3 mb-3 col-lg-4 ">
+                    <div class="bloc_ours ">
+                    
+                        <a href="#" class="text-decoration-none text-dark "></a>
+                            
+                             <img src="${teddy.imageUrl}" width="300" height="250" />
+                            
+                            <div class="descri">
+                                <div class="description">
+                    </div>
+                                   <h3> <a href="produits.html?${teddy._id}"> ${teddy.name}</a> </h3>
+                                    <p>${teddy.description}</p>
 
-          <img src="${teddy.imageUrl}" id="image" class="img-fluid col-6" alt="teddy_2" width="300" height="250">
-          <div class="col-6">
-            <h3 id="${teddy.name}"> nom ours </h3>
-            <p id="${teddy.description}">detail ours</p>
-            <div id="${teddy.price/100}.00€"> tarif </div>
-
-          </div>
-        </div>
-        <form>
-          <div class="form-row align-items-center">
-            <div class="col-auto my-1">
-              <label class="mr-sm-2" for="inlineFormCustomSelect">Preference</label>
-              <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
-                <option selected>Couleurs</option>
-                <option id="c1">One</option>
-                <option id="c2">Two</option>
-                <option id="c3">Three</option>
-                <option id="c4">Four</option>
-
-              </select>
-            </div>
-
-
-          </div>
-        </form>`
+                                </div>
+                                <div class="prix">
+                                    <strong>${teddy.price/100}.00€</strong>
+                                </div>
+                              
+                            </div>
+                        </a>
+                    </div>
+                </div>`
         }
 
-    })
+    })}
