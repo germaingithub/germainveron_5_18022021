caea6442559e5517f1128ae701464f89fa855da7
Germaingit <germain.veron@me.com>
2021-03-16 12:13:46 +0100

    ajout panier

--- frontend/descri.js
+++ frontend/descri.js
@@ -19,12 +19,15 @@ function getProductData(productId) {
                       <div class="row">
                         <img src="${productData.imageUrl}" id="image" class="img-fluid col-6" alt="teddy_2" width="300" height="250">
                           <div class="col-6">
-                            <h3> ${productData.name} </h3>
+                            <h3> ${productData.name} id="nameP" </h3>
                             <p>${productData.description}</p>
                           <div class="prix">
-                            <strong>${productData.price/100}.00€</strong>
+                            <strong>${productData.price/100}.00€ id="priceP"</strong>
                           </div>                              
                       </div>
+                          <button id="addtobasket" type="button" class="btn btn-primary btn-lg col-4 align-self-end mb-3"
+                            >Ajouter au panier
+                          </button>
                     </div>
                 `
          
@@ -38,5 +41,17 @@ function getProductData(productId) {
             <option value="${color}">${color}</option>
           `
       } 
+      function ajoutPanier(){
+    var btn = document.getElementByID("#addToBasket");
+    btn.addEventListener("click", async function(){
+      alert = "article ajouté"
+        cart.push();
+        localStorage.setitem('panier', JSON.stringify(panier));
+        alert("Ajouté au panier")
+        location.reload();
+    });
+};
+ajoutPanier();
     })
+    
 }
--- frontend/storage.js
+++ frontend/storage.js
@@ -1,5 +1,7 @@
+
+
 //panier user
-if (localStorage.getItem("panier")){
+if (localStorage.getItem("panierClient")){
     console.log("Panier OK");
 }else{
     console.log("Création du panier");
@@ -7,11 +9,65 @@ if (localStorage.getItem("panier")){
     localStorage.setItem("monPanier", (JSON.stringify(init)));
 }
 
-let panier = JSON.parse(localStorage.getItem("panier")); // stock le panier 
+let panier = JSON.parse(localStorage.getItem("panierClient")); // stock le panier 
 
 //le nombre d'article dans le panier 
-function nombreArticle (){
-    let numberArticle = document.getElementById("articleNumber");
-    numberArticle.textContent = panier.length;
+//function nombreArticle (){
+//    let numberArticle = document.getElementById("articleNumber");
+//    numberArticle.textContent = panier.length;
+//}
+//nombreArticle();
+
+
+let total= 0;
+function affichagePanier (){
+    if (panier.length> 0){
+        document.getElementById("panierVide").remove();
+//tableau recapitulant la commande
+        let tableauGeneral = document.getElementById("panier");
+        let tableauPanier = create ("table","class", "tableauPanier");
+        let tableauHeader = create ("tr", "class", "tableauHeader");
+        let tableauHeaderPic = document.createElement ("th");
+        let tableauHeaderName = document.createElement ("th");
+        let tableauHeaderPrice = document.createElement ("th");
+        let tableauHeaderAction = document.createElement ("th");
+        let tableauFooter = create ("tr", "class", "tableauufooter");
+        let tableauFooterTotalPrice = create("th","class","tableauFooterTotalPrice");
+        
+        tableauFooterTotalPrice.setattribute("colspan", "4")
+
+
+tableauGeneral.appendChild(tableauPanier);
+tableauPanier.appendChild(tableauHeader);
+tableauHeader.appendChild(tableauHeaderPic);
+tableauHeader.appendChild(tableauHeaderName);
+tableauHeader.appendChild(tableauHeaderPrice);
+tableauHeader.appendChild(tableauHeaderAction);
+
+tableauHeaderPic.textContent = "Vos Produit(s)";
+tableauHeaderName = "Nom";
+tableauHeaderPrice = "Tarif";
+tableauHeaderAction = "Action";
+
+JSON.parse(localStorage.getItem("panier-client")).forEach((article, index)=>{
+    let articleligne = create("tr", "id", "articleLigne");
+    let articlePic = create ("img","id","articlePic");
+    let articleName = create ("td", "id", "articleName");
+    let articlePrice = create ("td", "id", "articlePrice");
+    let articleAction = create ("i", "id", index);
+
+    articlePic.setattribute("src", article.imageURL);
+    articleAction.setattribute("alt", "Supprimer l'article");
+    articleAction.setattribute("class", "fas fa-trash-alt");
+
+    articleAction.addEventlistener("Click", function (event) {
+        suppressionArticle(event.target.id);
+    })
+
 }
-nombreArticle();
+
+
+
+
+)}
+}
